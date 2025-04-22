4b2215ce6f2ab149005595f6fae806921acf940c
Rizky598 <hakimrizky19@gmail.com>
2025-04-22 18:04:39 +0700

    Create Blackhead Rizky.py

---
+++ Blackhead Rizky.py
@@ -0,0 +1,286 @@
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+ #!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+ #!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
+#!/usr/python
+
+
+import os,sys,time
+os.system("clear")
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0m"     # White
+R = "\033[31m"    # Red
+C = "\033[36m"    # Cyan
+
+
+os.system("apt install mpv")
+os.system("mpv https://f.top4top.io/m_3399ah5fn0.mp3")
+os.system("clear")
+def load(word):
+        for char in word:
+                print(char,end="")
+                sys.stdout.flush()
